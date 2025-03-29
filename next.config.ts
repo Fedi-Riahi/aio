e408ad04b7a1d2561da1c1296f23bb7dd2d6a941
Aziz <Aziz@gmail.com>
2025-03-29 06:09:37 -0700

    Final Typos 4

--- next.config.ts
+++ next.config.ts
@@ -2,26 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['d1s8l6ybvn2guu.cloudfront.net', 'via.placeholder.com'],
+    domains: [ 'd1s8l6ybvn2guu.cloudfront.net','via.placeholder.com'],
   },
   async rewrites() {
     return [
       {
         source: "/api/init-payment",
-        destination: "http://localhost:3000/api/init-payment",
+        destination: "http://localhost:3041/api/init-payment",
       },
     ];
-  },
-  // Turbopack-specific configuration
-  experimental: {
-    turbo: {
-      rules: {
-        // Add rule for .txt files
-        '*.txt': {
-          loaders: ['raw-loader'],
-        },
-      },
-    },
   },
 };
 
