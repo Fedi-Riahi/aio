d5cbdd0900ecc92fcce91fed99e08cd9b80aca60
Fedi-Riahi <riahifedi@outlook.fr>
2025-04-08 12:26:38 +0100

    Bug

--- types/theatreView.ts
+++ types/theatreView.ts
@@ -1,43 +1,43 @@
+// ../types/theatreView.ts
+
 export interface Seat {
-    seat_index: string;
-    is_removed: boolean;
-    _id: string;
-  }
+  seat_index: string; // e.g., "A-1"
+  is_removed: boolean;
+  _id: string;
+}
 
-  export interface Order {
-    event_id: { id: string };
-    period_index: number;
-    location_index: number;
-    time_index: number;
-    ticketDataList?: { room_name: string; seat_index: string }[];
-    takenSeats?: string[] | Record<string, string[]>;
-  }
+export interface Order {
+  event_id: { id: string };
+  period_index: number;
+  location_index: number;
+  time_index: number;
+  ticketDataList?: { room_name: string; seat_index: string }[];
+  takenSeats?: string[] | Record<string, string[]>;
+}
 
-  export interface TheatreViewProps {
-    order: Order;
-    ticketIndex?: string;
-    seats: Seat[];
-    roomName: string;
-    selectedSeats: string[];
-    setSelectedSeats: React.Dispatch<React.SetStateAction<string[]>>;
-    maxSeats: number;
-    takenSeats: string[];
-  }
+export interface TheatreViewProps {
+  order: Order;
+  ticketIndex?: string;
+  seats: Seat[];
+  roomName: string;
+  selectedSeats: string[];
+  setSelectedSeats: React.Dispatch<React.SetStateAction<string[]>>;
+  maxSeats: number;
+  takenSeats: string[];
+}
 
-  export interface SeatMapProps {
-    seats: Seat[];
-    taken: string[];
-    selectedSeats: string[];
-    setSelectedSeats: React.Dispatch<React.SetStateAction<string[]>>;
-    containerWidth: number;
-    containerHeight: number;
-  }
+export interface SeatMapProps {
+  seats: Seat[];
+  taken: string[];
+  selectedSeats: string[];
+  setSelectedSeats: React.Dispatch<React.SetStateAction<string[]>>;
+  containerWidth: number;
+  containerHeight: number;
+}
 
-  export interface SeatProps {
-    seatId: string;
-    taken: boolean;
-    seatActive: boolean;
-    isRemoved: boolean;
-    seatSize: number;
-    onSelect: (seatId: string) => void;
-  }
+export interface SeatProps extends Seat {
+  taken: boolean;
+  seatActive: boolean;
+  seatSize: number;
+  onSelect: (seatId: string) => void;
+}
